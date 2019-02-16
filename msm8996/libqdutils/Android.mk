729e1a4cfdf4e40f0bee0f49f1d87176000daf67
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-02-16 21:34:05 +0530

    display: msm8996: Partially convert libqdutils makefile to blueprint
     * Required by CAF surfaceflinger
    
    Change-Id: I93125be0561a673a1f268957f3a3637cdd35a096
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- msm8996/libqdutils/Android.mk
+++ msm8996/libqdutils/Android.mk
@@ -16,20 +16,3 @@ LOCAL_SRC_FILES               := profiler.cpp \
                                  qd_utils.cpp \
                                  display_config.cpp
 include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_EXPORT_C_INCLUDE_DIRS   := $(LOCAL_PATH)
-LOCAL_HEADER_LIBRARIES          := libhardware_headers
-LOCAL_SHARED_LIBRARIES          := liblog libcutils
-LOCAL_C_INCLUDES                := $(common_includes)
-LOCAL_ADDITIONAL_DEPENDENCIES   := $(common_deps)
-LOCAL_SRC_FILES                 := qdMetaData.cpp
-LOCAL_CFLAGS                    := $(common_flags) -Wno-sign-conversion
-LOCAL_CFLAGS                    += -DLOG_TAG=\"DisplayMetaData\"
-LOCAL_CLANG                     := true
-LOCAL_MODULE_TAGS               := optional
-LOCAL_MODULE                    := libqdMetaData
-LOCAL_PROPRIETARY_MODULE        := true
-include $(BUILD_SHARED_LIBRARY)
-
